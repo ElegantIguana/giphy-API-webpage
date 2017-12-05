849e2d3bb192204d4e15c794aec96423182107ba
Armand Guim <armandguim3@gmail.com>
2017-12-05 02:37:38 -0500

    added dynamic buttons + search bar

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,9 +1,21 @@
-$("button").on("click", function() {
+$(document).ready(function() {
 
 //Creates an array of items for the query
 var reactions = ["excited", "mad", "angry", "joy", "surprised", "wow", "sad", "depressed", "gloomy", "confused", "whatever", "love", "thank you", "thanks", "crying", "cry", "k", "popcorn", "flirting", "flirty", "celebration", "bored", "dance"];
 //Made this line to check if this file was linked
 console.log(reactions);
+
+for (var i = 0; i < reactions.length; i++) {
+	var reactionButtons = $("<button>" + reactions[i] + "</button>")
+	reactionButtons.attr("value", reactions[i])
+	reactionButtons.appendTo("#buttons");
+}
+	$("form").submit(function(){
+    alert("Submitted");
+    //TODO: add code to grab data from the form
+	});
+
+	$("button").on("click", function() {
 
 //stores the value of the buttons in a container
 var reaction = $(this).attr("data-reaction");
@@ -35,7 +47,9 @@ $.ajax({
         		$("#gifs-appear-here").prepend(gifDiv);
            	}
 
-
+           	//TODO: add pause and play functionality for each gif
+           	//(see pausingGifs example)
 
         });
     });
+});
